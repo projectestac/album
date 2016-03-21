b089a887d5728e3505bec11ed58419428cad9df7
Francesc Busquets <francesc@gmail.com>
2016-03-22 00:17:43 +0100

    Implementing JQuery-UI draggable

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,35 +1,46 @@
 
-/* global chrome */
+/* global chrome, clipboard */
 
 // Load when the DOM is ready to be used
 $(function () {
 
-  $('#stopBtn').html('stop').click(function () {
-    chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
-  });
-  
-  var $list = $('ul#list');
+    $('#stopBtn').html('stop').click(function () {
+        chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
+    });
 
-  chrome.runtime.onMessage.addListener(
-          function (request, sender, sendResponse) {
-            if (request.imgurl) {
-                var $lispan = $('<span/>');
-                $lispan.append($('<input type="checkbox" checked/>'));
-                $lispan.append($('<img class="imgcaption" src="'+request.imgurl+'"/>'));
-                $lispan.append($('<span class="urltext">'+request.imgurl+'</span>'));
-                $list.append($('<li/>').data('url', request.imgurl).append($lispan));
-            }
-          });
-          
-  $('#copyBtn').html('copy').click(function () {
-      var txt='';
-      $('li').each(function(index){
-          txt=txt+($(this).data('url'))+ '\n';
-      });
-      clipboard.copy(txt);
-  });
-          
-          
-  chrome.tabs.executeScript(null, {file: 'listimages.js'});
+    var $list = $('ul#list')
+            .sortable({placeholder: "ui-state-highlight"})
+            .disableSelection();
+
+    chrome.runtime.onMessage.addListener(
+            function (request, sender, sendResponse) {
+                if (request.imgurl) {
+                    var $lispan = $('<span/>');
+                    $lispan.append($('<input class="ui-state-default" type="checkbox" checked/>'));
+                    $lispan.append($('<img class="imgcaption" src="' + request.imgurl + '"/>'));
+                    $lispan.append($('<span class="urltext">' + request.imgurl + '</span>'));
+                    $list.append($('<li class="ui-state-default ui-sortable-handle"/>')
+                            .data('url', request.imgurl)
+                            .append($lispan));
+                }
+            });
+
+    $('#copyListBtn').html('copy').click(function () {
+        var txt = '';
+        $('li').each(function (index) {
+            txt = txt + ($(this).data('url')) + '\n';
+        });
+        clipboard.copy(txt);
+    });
+
+    $('#copyHtmlBtn').html('copy').click(function () {
+        var txt = '';
+        $('li').each(function (index) {
+            txt=txt + '<img src="' + ($(this).data('url')) + '">\n';
+        });
+        clipboard.copy(txt);
+    });
+
+    chrome.tabs.executeScript(null, {file: 'listimages.js'});
 
 });

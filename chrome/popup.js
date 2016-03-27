76b654d0184a655b13509453c1c246838b46d8e3
Francesc Busquets <francesc@gmail.com>
2016-03-27 21:12:19 +0200

    Generating code for Galleria [http://galleria.io]

--- chrome/popup.js
+++ chrome/popup.js
@@ -23,14 +23,14 @@ $(function () {
   $('#copyListBtn').html(chrome.i18n.getMessage('copyListBtn')).prop('title', chrome.i18n.getMessage('copyListBtnTooltip'));
   $('#copyHtmlBtn').html(chrome.i18n.getMessage('copyHtmlBtn')).prop('title', chrome.i18n.getMessage('copyHtmlBtnTooltip'));
   $('#copyScriptBtn').html(chrome.i18n.getMessage('copyScriptBtn')).prop('title', chrome.i18n.getMessage('copyScriptBtnTooltip'));
-  
+
   /**
    * Not used
    * 
-  $('#stopBtn').html('stop').click(function () {
-    chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
-  });
-  */
+   $('#stopBtn').html('stop').click(function () {
+   chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
+   });
+   */
 
   var $list = $('ul#list')
           .sortable({placeholder: "ui-state-highlight"})
@@ -83,28 +83,27 @@ $(function () {
     copyAndNotify(txt);
   });
 
-  // See 'slideshow.js' in /misc
-  var slideShowCode = 'function createSlideShow(){for(var i=jQuery,e=i(\'<div id="slideshow"/>\').css({display:"block",width:"fit-content",margin:"auto"}),t=\'<svg fill="#222222" width="24" height="24" viewBox="0 0 24 24">\',d=i(\'<div id="ssPrevBtn">\').css({display:"inline-block","vertical-align":"middle"}).html(t+\'<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/></svg>\'),s=i(\'<div id="ssNextBtn">\').css({display:"inline-block","vertical-align":"middle"}).html(t+\'<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>\'),a=i(\'<div id="ssSlides"/>\').css({display:"inline-block","vertical-align":"middle",position:"relative",padding:"10px","box-shadow":"0 0 20px rgba(0,0,0,0.4)",width:slideShowOptions.width,height:slideShowOptions.height}),n="10px",l=0;l<slideShowOptions.pics.length;l++){var o=i("<div/>").css({position:"absolute",top:n,left:n,right:n,bottom:n}).append(i(\'<img src="\'+slideShowOptions.pics[l]+\'"/>\').css({position:"absolute",top:0,bottom:0,left:0,right:0,"max-width":"100%","max-height":"100%",margin:"auto"}));l>0&&o.hide(),a.append(o)}i("#"+slideShowOptions.playerid).append(e.append(d).append(a).append(s));var p=function(i){jQuery("#ssSlides > div:first").fadeOut(i).next().fadeIn(i).end().appendTo("#ssSlides")},c=function(i){jQuery("#ssSlides > div:first").fadeOut(i).nextAll().last().prependTo("#ssSlides").fadeIn(i)},r=0,h=function(i){r&&(window.clearInterval(r),r=0),i&&(r=window.setInterval(p,3e3))};d.on("click",function(){h(!1),c(0)}),s.on("click",function(){h(!1),p(0)}),slideShowOptions.autoplay&&h(!0)}if(document.writeln(\'<div id="\'+slideShowOptions.playerid+\'"/>\'),"undefined"==typeof jQuery){var headTag=document.getElementsByTagName("head")[0],jqTag=document.createElement("script");jqTag.type="text/javascript",jqTag.src="//ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js",jqTag.onload=createSlideShow,headTag.appendChild(jqTag)}else createSlideShow();';
-
   $('#copyScriptBtn').click(function () {
-    var txt = '[raw]\n<script type="text/javascript">\n' +
-            'var slideShowOptions = {\n' +
-            'autoplay: true,\n' +
-            'width: \'420px\',\n' +
-            'height: \'267px\',\n' +
-            'playerid: \''+ Math.floor(Math.random()*100000) +'\',\n' +
-            'pics: [\n';
-    var first = true;
+    
+    // Gallery made with http://galleria.io/
+    
+    var playerId = Math.floor(Math.random() * 65535).toString(16);
+    var width = 600, height = 400;
+    var txt = '[raw]\n' +
+            '<script type="text/javascript" src="//cdn.jsdelivr.net/g/jquery@2.2.2,galleria@1.4.2(galleria.js)"></script>\n' +
+            '<div id="' + playerId + '" style="width:' + width + 'px; height:' + height + 'px;">\n';
     $('li').each(function (index) {
-      if (selected[index]){
-        if(!first)
-          txt = txt + ',\n';
-        else
-          first = false;
-        txt = txt + '\'' + $(this).data('url') + '\'';
+      if (selected[index]) {
+        txt = txt + '<img src="' + $(this).data('url') + '">\n';
       }
     });
-    txt = txt + ']};\n' + slideShowCode + '\n</script>\n[/raw]';
+    txt = txt +
+            '</div>\n' +
+            '<script>\n' +
+            'Galleria.loadTheme(\'//cdn.jsdelivr.net/galleria/1.4.2/themes/classic/galleria.classic.js\');\n' +
+            'Galleria.run(\'#' + playerId + '\');\n' +
+            '</script>\n' +
+            '[/raw]';
     copyAndNotify(txt);
   });
 

cffca37fe469bef760ad13c1bd258cc80dfa2a55
Francesc Busquets <francesc@gmail.com>
2016-03-29 12:28:39 +0200

    Exporting links to galleria.io

--- chrome/listimages.js
+++ chrome/listimages.js
@@ -1,142 +1,152 @@
+/**
+ * listimages.js
+ * 
+ * 
+ */
+
 
 /* global chrome */
 
 var ListImages = function () {
-    this.allImages = [];
-    this.allLinks = [];
+  this.allImages = [];
+  this.allLinks = [];
 };
 
 ListImages.prototype = {
-    constructor: ListImages,
-    allImages: null,
-    allLinks: null,
-    scanProcess: null,
-    scanning: false,
-    reporting: false,
-    SCAN_INTERVAL: 1000,
-    scanImages: function () {
+  constructor: ListImages,
+  allImages: null,
+  allLinks: null,
+  scanProcess: null,
+  scanning: false,
+  reporting: false,
+  SCAN_INTERVAL: 1000,
+  scanImages: function () {
 
-        // Avoid re-entrant processing
-        if (this.scanning || this.reporting)
-            return;
+    // Avoid re-entrant processing
+    if (this.scanning || this.reporting)
+      return;
 
-        this.scanning = true;
-        console.log('scanning...');
+    this.scanning = true;
+    console.log('scanning...');
 
-        var imgList = [];
-        var objList = [];
+    var imgList = [];
+    var objList = [];
 
-        // Check `img` objects
-        var obj = document.querySelectorAll('img');
-        for (var n = 0; n < obj.length; n++) {
-            if (obj[n].src){
-                objList.push(obj[n]);
-                imgList.push(obj[n].src);
-            }
-        }
-
-        // Check all objects with `background-image` attribute
-        obj = document.querySelectorAll('div[style*=background-image]');
-        for (var n = 0; n < obj.length; n++) {
-            var exp = window.getComputedStyle(obj[n]).getPropertyValue('background-image').trim();
-            if (exp.toLowerCase().substring(0, 4) === 'url(') {
-                exp = exp.substring(4, exp.length - 1).trim();
-                // Remove enclosing quotes
-                if ((exp.substring(0, 1) === '"' && exp.substring(exp.length - 1, exp.length) === '"') ||
-                        (exp.substring(0, 1) === '\'' && exp.substring(exp.length - 1, exp.length) === '\'')) {
-                    exp = exp.substring(1, exp.length - 1).trim();
-                }
-                imgList.push(exp);
-                objList.push(obj[n]);
-            }
-        }
-
-        // Filter invalid URLs
-        for (var n = 0; n < imgList.length; n++) {
-            try {
-                var exp = imgList[n];
-                var url = new URL(exp);
-                if (url && this.allImages.indexOf(exp) === -1 && url.protocol &&
-                        (url.protocol === 'http:' || url.protocol === 'https:')) {
-                    this.allImages.push(exp);
-                    var obj = {imgurl: exp};
-                    var link = findLinkUp(objList[n]);
-                    if(link===null)
-                        link = findLinkDown(objList[n]);
-                    if(link){
-                        this.allLinks[this.allImages.length-1]=link;
-                        obj.imglink = link;
-                    }
-                    
-                    // Notify plugin
-                    chrome.runtime.sendMessage(obj);
-                }
-            } catch (ex) {
-                console.log('Error processing element ' + n + ':\n' + exp + ' - ' + ex);
-            }
-        }
-        this.scanning = false;
-    },
-    startScanning: function () {
-        if (this.scanProcess)
-            this.endScanning();
-        var thisObj = this;
-        this.scanProcess = window.setInterval(function () {
-            thisObj.scanImages();
-        }, this.SCAN_INTERVAL);
-    },
-    endScanning: function () {
-        if (this.scanProcess) {
-            window.clearInterval(this.scanProcess);
-            this.scanProcess = null;
-            console.log('scanning stopped!');
-        }
-    },
-    listScannedImages: function () {
-        this.reporting = true;
-        for (var i = 0; i < this.allImages.length; i++) {
-            var obj = {imgurl: this.allImages[i]};
-            if(this.allLinks[i])
-                obj.imglink = this.allLinks[i];            
-            chrome.runtime.sendMessage(obj);
-        }
-        this.reporting = false;
+    // Check `img` objects
+    var obj = document.querySelectorAll('img');
+    for (var n = 0; n < obj.length; n++) {
+      if (obj[n].src) {
+        objList.push(obj[n]);
+        imgList.push(obj[n].src);
+      }
     }
+
+    // Check all objects with `background-image` attribute
+    obj = document.querySelectorAll('div[style*=background-image]');
+    for (var n = 0; n < obj.length; n++) {
+      var exp = window.getComputedStyle(obj[n]).getPropertyValue('background-image').trim();
+      if (exp.toLowerCase().substring(0, 4) === 'url(') {
+        exp = exp.substring(4, exp.length - 1).trim();
+        // Remove enclosing quotes
+        if ((exp.substring(0, 1) === '"' && exp.substring(exp.length - 1, exp.length) === '"') ||
+                (exp.substring(0, 1) === '\'' && exp.substring(exp.length - 1, exp.length) === '\'')) {
+          exp = exp.substring(1, exp.length - 1).trim();
+        }
+        imgList.push(exp);
+        objList.push(obj[n]);
+      }
+    }
+
+    // Filter invalid URLs
+    for (var n = 0; n < imgList.length; n++) {
+      try {
+        var exp = imgList[n];
+        var url = new URL(exp);
+        if (url && this.allImages.indexOf(exp) === -1 && url.protocol &&
+                (url.protocol === 'http:' || url.protocol === 'https:')) {
+          this.allImages.push(exp);
+          var obj = {imgurl: exp};
+          var link = findLinkUp(objList[n]);
+          if (link === null)
+            link = findLinkDown(objList[n]);
+          if (link) {
+            this.allLinks[this.allImages.length - 1] = link;
+            obj.imglink = link;
+          }
+
+          // Notify plugin
+          chrome.runtime.sendMessage(obj);
+        }
+      } catch (ex) {
+        console.log('Error processing element ' + n + ':\n' + exp + ' - ' + ex);
+      }
+    }
+    this.scanning = false;
+  },
+  startScanning: function () {
+    if (this.scanProcess)
+      this.endScanning();
+    var thisObj = this;
+    this.scanProcess = window.setInterval(function () {
+      thisObj.scanImages();
+    }, this.SCAN_INTERVAL);
+  },
+  endScanning: function () {
+    if (this.scanProcess) {
+      window.clearInterval(this.scanProcess);
+      this.scanProcess = null;
+      console.log('scanning stopped!');
+    }
+  },
+  listScannedImages: function () {
+    this.reporting = true;
+    for (var i = 0; i < this.allImages.length; i++) {
+      var obj = {imgurl: this.allImages[i]};
+      if (this.allLinks[i])
+        obj.imglink = this.allLinks[i];
+      chrome.runtime.sendMessage(obj);
+    }
+    this.reporting = false;
+  }
 };
 
+var alreadyLooked = [];
+
 var findLinkUp = function (obj) {
-    if (obj.nodeName.toLowerCase === 'a') {
-        console.log('Found "a" '+obj);
-        var href = obj.getAttribute('href');
-        if (href)
-            return href;
-    }
-    var parent = obj.parentElement;
-    return parent ? this.findLinkUp(parent) : null;
+  if (obj.nodeName.toLowerCase() === 'a') {
+    if (obj.getAttribute('href'))
+      return absoluteLink(obj);
+  }
+  if (alreadyLooked.indexOf(obj) === -1)
+    alreadyLooked.push(obj);
+  var parent = obj.parentElement;
+  return parent ? this.findLinkUp(parent) : null;
 };
 
 var findLinkDown = function (obj) {
-    if (obj.nodeName.toLowerCase === 'a') {
-        console.log('Found "a" '+obj);
-        var href = obj.getAttribute('href');
-        if (href)
-            return href;
+  if (obj.nodeName.toLowerCase() === 'a') {
+    if (obj.getAttribute('href'))
+      return absoluteLink(obj);
+  }
+  for (var i = 0; i < obj.children.length; i++) {
+    var child = obj.children.item(i);
+    if (child) {
+      var link = findLinkDown(child);
+      if (link)
+        return link;
     }
-    for (var i = 0; i < obj.children.length; i++) {
-        var child = obj.children.item(i);
-        if (child) {
-            var link = findLinkDown(child);
-            if (link)
-                return link;
-        }
-    }
-    return  null;
+  }
+  return  null;
 };
+
+var absoluteLink = function (link) {
+  return link.protocol ? link.protocol + "//" + link.host + link.pathname + link.search + link.hash : null;
+}
 
 
 if (typeof window.__listImages === 'undefined')
-    window.__listImages = new ListImages();
+  window.__listImages = new ListImages();
 else
-    window.__listImages.listScannedImages();
+  window.__listImages.listScannedImages();
 
 window.__listImages.startScanning();
--- chrome/popup.js
+++ chrome/popup.js
@@ -4,120 +4,117 @@
 // Load when the DOM is ready to be used
 $(function () {
 
-    var numImgs = 0;
-    var numSelected = 0;
-    var selected = [];
+  var numImgs = 0;
+  var numSelected = 0;
+  var selected = [];
 
-    var updateNumSelected = function () {
-        var result = 0;
-        for (var i = 0; i < numImgs; i++)
-            if (selected[i])
-                result++;
-        numSelected = result;
-        return result;
-    };
+  var updateNumSelected = function () {
+    var result = 0;
+    for (var i = 0; i < numImgs; i++)
+      if (selected[i])
+        result++;
+    numSelected = result;
+    return result;
+  };
 
-    $('.description').html(chrome.i18n.getMessage('extDescText'));
-    $('#numImgLb').html(chrome.i18n.getMessage('numImgLb'));
-    $('#selImgLb').html(chrome.i18n.getMessage('selImgLb'));
-    $('#copyListBtn').html(chrome.i18n.getMessage('copyListBtn')).prop('title', chrome.i18n.getMessage('copyListBtnTooltip'));
-    $('#copyHtmlBtn').html(chrome.i18n.getMessage('copyHtmlBtn')).prop('title', chrome.i18n.getMessage('copyHtmlBtnTooltip'));
-    $('#copyScriptBtn').html(chrome.i18n.getMessage('copyScriptBtn')).prop('title', chrome.i18n.getMessage('copyScriptBtnTooltip'));
+  $('.description').html(chrome.i18n.getMessage('extDescText'));
+  $('#numImgLb').html(chrome.i18n.getMessage('numImgLb'));
+  $('#selImgLb').html(chrome.i18n.getMessage('selImgLb'));
+  $('#copyListBtn').html(chrome.i18n.getMessage('copyListBtn')).prop('title', chrome.i18n.getMessage('copyListBtnTooltip'));
+  $('#copyHtmlBtn').html(chrome.i18n.getMessage('copyHtmlBtn')).prop('title', chrome.i18n.getMessage('copyHtmlBtnTooltip'));
+  $('#copyScriptBtn').html(chrome.i18n.getMessage('copyScriptBtn')).prop('title', chrome.i18n.getMessage('copyScriptBtnTooltip'));
 
-    /**
-     * Not used
-     * 
-     $('#stopBtn').html('stop').click(function () {
-     chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
-     });
-     */
+  $('#stopBtn').html('stop').click(function () {
+    chrome.tabs.executeScript(null, {code: 'window.__listImages.endScanning();'});
+  });
 
-    var $list = $('ul#list')
-            .sortable({placeholder: "ui-state-highlight"})
-            .disableSelection();
+  var $list = $('ul#list')
+          .sortable({placeholder: "ui-state-highlight"})
+          .disableSelection();
 
-    chrome.runtime.onMessage.addListener(
-            function (request, sender, sendResponse) {
-                if (request.imgurl) {
-                    var n = numImgs;
-                    selected[n] = true;
-                    var $lispan = $('<span/>');
-                    $lispan.append($('<input class="ui-state-default" type="checkbox" checked/>').change(function () {
-                        selected[n] = this.checked ? true : false;
-                        $('#numSel').html(updateNumSelected());
-                    }));
-                    $lispan.append($('<img class="imgcaption" src="' + request.imgurl + '"/>'));
-                    $lispan.append($('<span class="urltext">' + request.imgurl + '</span>'));
-                    var $li = $('<li class="ui-state-default ui-sortable-handle"/>')
-                            .data('url', request.imgurl)
-                            .append($lispan);
-                    if (request.imglink)
-                        $li.data('link', request.imglink);
-                    $list.append($li);
-                    $('#numImgs').html(++numImgs);
-                    $('#numSel').html(++numSelected);
-                }
-            });
-
-    var copyAndNotify = function (txt) {
-        clipboard.copy(txt);
-        chrome.notifications.create({
-            type: 'basic',
-            title: chrome.i18n.getMessage('extName'),
-            message: chrome.i18n.getMessage('msgDataCopied'),
-            iconUrl: 'icons/icon192.png'});
-    };
-
-    $('#copyListBtn').click(function () {
-        var txt = '';
-        $('li').each(function (index) {
-            if (selected[index])
-                txt = txt + ($(this).data('url')) + '\n';
-        });
-        copyAndNotify(txt);
-    });
-
-    $('#copyHtmlBtn').click(function () {
-        var txt = '';
-        $('li').each(function (index) {
-            if (selected[index]) {
-                var imgText = '<img src="' + ($(this).data('url')) + '">';
-                var link = $(this).data('link');
-                if (link) {
-                    imgText = '<a href="' + link + '">' + imgText + '</a>';
-                }
-                txt = txt + imgText + '\n';
+  chrome.runtime.onMessage.addListener(
+          function (request, sender, sendResponse) {
+            if (request.imgurl) {
+              var n = numImgs;
+              selected[n] = true;
+              var $lispan = $('<span/>');
+              $lispan.append($('<input class="ui-state-default" type="checkbox" checked/>').change(function () {
+                selected[n] = this.checked ? true : false;
+                $('#numSel').html(updateNumSelected());
+              }));
+              $lispan.append($('<img class="imgcaption" src="' + request.imgurl + '"/>'));
+              $lispan.append($('<span class="urltext">' + request.imgurl + '</span>'));
+              var $li = $('<li class="ui-state-default ui-sortable-handle"/>')
+                      .data('url', request.imgurl)
+                      .append($lispan);
+              if (request.imglink)
+                $li.data('link', request.imglink);
+              $list.append($li);
+              $('#numImgs').html(++numImgs);
+              $('#numSel').html(++numSelected);
             }
-        });
-        copyAndNotify(txt);
+          });
+
+  var copyAndNotify = function (txt) {
+    clipboard.copy(txt);
+    chrome.notifications.create({
+      type: 'basic',
+      title: chrome.i18n.getMessage('extName'),
+      message: chrome.i18n.getMessage('msgDataCopied'),
+      iconUrl: 'icons/icon192.png'});
+  };
+
+  var listImages = function (withImg, withLinks, dataLink) {
+    var result = '';
+    $('li').each(function (index) {
+      if (selected[index]) {
+        var txt = $(this).data('url');
+        if (withImg)
+          txt = '<img src="' + txt + '">';
+        if (withLinks) {
+          var link = $(this).data('link');
+          if (link) {
+            if(dataLink)
+              txt = txt.slice(0, -1) + ' data-link="' + link + '">';
+            else
+              txt = '<a href="' + link + '">' + txt + '</a>';
+          }
+        }
+        result = result + txt + '\n';
+      }
     });
+    return result;
+  };
 
-    $('#copyScriptBtn').click(function () {
+  $('#copyListBtn').click(function () {
+    copyAndNotify(listImages(false, false, false));
+  });
 
-        // Gallery made with http://galleria.io/
+  $('#copyHtmlBtn').click(function () {
+    copyAndNotify(listImages(true, true, false));
+  });
 
-        var playerId = Math.floor(Math.random() * 100000).toString(16).toUpperCase();
-        var width = 600, height = 400;
-        var txt = '[raw]\n' +
-                '<script type="text/javascript" src="//cdn.jsdelivr.net/g/jquery@1.12.1,galleria@1.4.2(galleria.js)"></script>\n' +
-                '<div id="' + playerId + '" style="width: ' + width + 'px; height: ' + height + 'px;">\n';
-        $('li').each(function (index) {
-            if (selected[index]) {
-                txt = txt + '<img src="' + $(this).data('url') + '">\n';
-            }
-        });
-        txt = txt +
-                '</div>\n' +
-                '<script>\n' +
-                'Galleria.loadTheme(\'//cdn.jsdelivr.net/galleria/1.4.2/themes/classic/galleria.classic.js\');\n' +
-                'Galleria.run(\'#' + playerId + '\', {\n' +
-                //'  width: ' + width + ', height:' + height + '\n'+
-                '});\n' +
-                '</script>\n' +
-                '[/raw]';
-        copyAndNotify(txt);
-    });
+  $('#copyScriptBtn').click(function () {
 
-    chrome.tabs.executeScript(null, {file: 'listimages.js'});
+    // Gallery made with http://galleria.io/
+
+    var playerId = Math.floor(Math.random() * 100000).toString(16).toUpperCase();
+    var width = 600, height = 400;
+    copyAndNotify(
+            '[raw]\n' +
+            '<script type="text/javascript" src="https://cdn.jsdelivr.net/g/jquery@1.12.1,galleria@1.4.2(galleria.js)"></script>\n' +
+            '<div id="' + playerId + '" style="width: ' + width + 'px; height: ' + height + 'px;">\n' +
+            listImages(true, true, true) +
+            '</div>\n' +
+            '<script>\n' +
+            'Galleria.loadTheme(\'https://cdn.jsdelivr.net/galleria/1.4.2/themes/classic/galleria.classic.js\');\n' +
+            'Galleria.run(\'#' + playerId + '\', {\n' +
+            //'  width: ' + width + ', height:' + height + '\n'+
+            '});\n' +
+            '</script>\n' +
+            '[/raw]');
+  });
+
+  chrome.tabs.executeScript(null, {file: 'listimages.js'});
 
 });
